dea43a8688ba61291a3faeb0a2c80f11c6f19ef5
lohith357 <gaddamlohith.123@gmail.com>
2026-08-22 09:40:35 +0530

    ✅ Java Lambda Expressions [java8] — approach1

--- solutions/java8/medium/java-lambda-expressions/solution-approach1.java
+++ solutions/java8/medium/java-lambda-expressions/solution-approach1.java
@@ -6,7 +6,7 @@
 // Platform    HackerRank
 // Language    java8
 // Status      Accepted
-// Submitted   2026-08-21, 09:48 a.m.
+// Submitted   2026-08-22, 09:40 a.m.
 // ──────────────────────────────────────────────────
 
 
