67a816ba644a44a4dcad60222a26082387857191
lohith357 <gaddamlohith.123@gmail.com>
2026-08-22 09:22:18 +0530

    ✅ Sorting: Comparator [java8] — approach1

---
+++ solutions/java8/medium/ctci-comparator-sorting/solution-approach1.java
@@ -0,0 +1,24 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?isFullScreen=true
+// Problem     Sorting: Comparator
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-22, 09:22 a.m.
+// ──────────────────────────────────────────────────
+
+
+
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+             if (a.score != b.score) {
+            return b.score - a.score;
+        }
+        return a.name.compareTo(b.name);
+    
+    }
+}
+
